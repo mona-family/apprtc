8fd812993f5335bb8b66a24d44a84d9b72110e95
Niels Avonds <niels@nobi.life>
2021-02-04 23:06:12 +0100

    Updated ICE_SERVER_URL. Added TODO list

--- src/app_engine/constants.py
+++ src/app_engine/constants.py
@@ -38,7 +38,8 @@ ICE_SERVER_OVERRIDE = None
 #   }
 # ]
 
-ICE_SERVER_BASE_URL = 'https://appr.tc'
+# ICE_SERVER_BASE_URL = 'https://appr.tc'
+ICE_SERVER_BASE_URL = 'http://localhost:8080'
 ICE_SERVER_URL_TEMPLATE = '%s/v1alpha/iceconfig?key=%s'
 ICE_SERVER_API_KEY = os.environ.get('ICE_SERVER_API_KEY')
 HEADER_MESSAGE = os.environ.get('HEADER_MESSAGE')
